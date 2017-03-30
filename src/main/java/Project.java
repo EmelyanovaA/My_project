cffa63518a797f21c45bdd6b0ebaf1c421af3510
EmelyanovaA <Hip6@mail.ru>
2017-03-30 21:57:02 +0400

    My_project
    my_project

--- src/main/java/Project.java
+++ src/main/java/Project.java
@@ -2,32 +2,12 @@
 import java.sql.*;
 
 public class Project {
-    public static  void main(String args[]){
-        String url = "jdbc:postgresql://localhost:5432/postgres";
-        String user = "postgres";
-        String pass = "12345";
-        Connection conn = null;
-
-        try {
-            Class.forName("org.postgresql.Driver");
-            conn = DriverManager.getConnection(url, user, pass);
-            Statement stat = conn.createStatement();
-            ResultSet res = stat.executeQuery("select * from project");
-            int i = 1;
-            while(res.next()){
-                System.out.println("#"+i+"-"+res.getInt("id")+" "+res.getString(2));
-                i++;
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            if(conn!=null){
-                try {
-                    conn.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
+    public static  void main(String args[]) {
+        int id =555555;
+        Image image = new Image();
+        RealisationDAO rdao = new RealisationDAO();
+        System.out.println(rdao.getImage(id));
+        System.out.println(rdao.deleteImage(id));
+        //System.out.println(rdao.saveImage(image));
     }
 }
--- src/main/java/RealisationDAO.java
+++ src/main/java/RealisationDAO.java
@@ -1,15 +1,100 @@
+import java.sql.*;
+import java.text.DateFormat;
+
+import static javafx.scene.input.KeyCode.F;
 
 public class RealisationDAO implements ImageDAO {
 
     public Image getImage(int id) {
+        String url = "jdbc:postgresql://localhost:5432/postgres";
+        String user = "postgres";
+        String pass = "12345";
+        Connection conn = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            conn = DriverManager.getConnection(url, user, pass);
+            Statement stat = conn.createStatement();
+            ResultSet res = stat.executeQuery("select *from project where id = "+id);
+            int i = 1;
+            while(res.next()){
+                System.out.println("#"+i+"-"+res.getInt("id"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if(conn!=null){
+                try {
+                    conn.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
         return null;
     }
 
     public boolean saveImage(Image image) {
+        String url = "jdbc:postgresql://localhost:5432/postgres";
+        String user = "postgres";
+        String pass = "12345";
+        Connection conn = null;
+        Image im = new Image();
+        try {
+            Class.forName("org.postgresql.Driver");
+            conn = DriverManager.getConnection(url, user, pass);
+            String sql = "insert into project(id) values (?)";
+            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+            preparedStatement.setInt(1, 955555);
+            preparedStatement.executeUpdate();
+            Statement stat = conn.createStatement();
+            ResultSet res = stat.executeQuery("select *from project");
+            int i = 1;
+            while(res.next()) {
+                System.out.println("#" + i + "-" + res.getInt("id"));
+                i++;
+            }
+        }catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if(conn!=null){
+                try {
+                    conn.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
         return false;
     }
 
     public boolean deleteImage(int id) {
+        String url = "jdbc:postgresql://localhost:5432/postgres";
+        String user = "postgres";
+        String pass = "12345";
+        Connection conn = null;
+
+        try {
+            Class.forName("org.postgresql.Driver");
+            conn = DriverManager.getConnection(url, user, pass);
+            Statement stat = conn.createStatement();
+            stat.executeUpdate("delete from project where id ="+id);
+            ResultSet res = stat.executeQuery("select *from project");
+            int i = 1;
+            while(res.next()) {
+                System.out.println("#" + i + "-" + res.getInt("id"));
+                i++;
+            }
+        }catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+        if(conn!=null){
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
         return false;
     }
 }
